47aaf32a0941f9b5702f17fb490c337d390ec6bf
AnhLT <tuananh220695@gmail.com>
2024-07-18 11:46:08 +0700

    chore: save point

--- app.py
+++ app.py
@@ -3,11 +3,5 @@ from src.config import APP_HOST, APP_PORT
 
 app = create_app()
 
-
-@app.route("/health")
-def health():
-    return '{"status":true}', 200
-
-
 if __name__ == "__main__":
     app.run(host=APP_HOST, port=APP_PORT)
--- src/views/routes.py
+++ src/views/routes.py
@@ -11,3 +11,7 @@ def build_routes(app: Flask) -> None:
     app.register_blueprint(category_app, url_prefix="/api/v1/categories")
     app.register_blueprint(product_app, url_prefix="/api/v1/products")
     app.register_blueprint(user_app, url_prefix="/api/v1/users")
+
+    @app.route("/health")
+    def health():
+        return '{"status":true}', 200
